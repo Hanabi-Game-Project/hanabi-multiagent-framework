5b1a0e5f851b7c054fe1add334936ceecdfcc24e
Anton Komissarov <anton.v.komissarov@gmail.com>
2020-03-15 10:49:03 +0100

    rename project in setup and readme; bump version

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from skbuild import setup
 
 setup(
-    name='hanabi_framework',
-    version='0.0.1',
+    name='hanabi_multiagent_framework',
+    version='0.0.2',
     description='A framework for managing a hanabi game and agents.',
-    author='braintimeException/hanabi_framework',
-    packages=['hanabi_framework']
+    author='braintimeException/hanabi_multiagent_framework',
+    packages=['hanabi_multiagent_framework']
 )
