1b2299321e5dfa690be53c642f373f48c525445f
Anton Komissarov <anton.v.komissarov@gmail.com>
2020-04-21 18:07:17 +0200

    clean-up and incorporate changes from hanabi learning environment.

--- hanabi_multiagent_framework/environment.py
+++ hanabi_multiagent_framework/environment.py
@@ -1,21 +1,23 @@
 """
-This file defines a wrapper for HanabiEnv which overrides and extends the functionality of the
-rl_env.HanabiEnv.
+This file implements a wrapper for hanabi_learning_environment.HanabiParallelEnv.
 """
 
-from hanabi_learning_environment import rl_env
-from hanabi_learning_environment import pyhanabi
-import dm_env
-from dm_env import specs
+from typing import Tuple, Dict, List
 import numpy as np
+from dm_env import specs as dm_specs
+from hanabi_learning_environment import pyhanabi
 
 class HanabiParallelEnvironment:
-    """Hanabi environment wrapper for use with HanabiGameManager.
+    """Hanabi parallel environment wrapper for use with HanabiParallelSession.
     """
-    def __init__(self, env_config: dict, n_parallel: int):
-        self._parallel_env = pyhanabi.HanabiParallelEnv(env_config, n_parallel, True)
+    def __init__(self, env_config: Dict[str, str], n_parallel: int):
+        self._parallel_env = pyhanabi.HanabiParallelEnv(env_config, n_parallel)
+        self.n_players = self._parallel_env.parent_game.num_players()
 
-    def step(self, action_ids: list, agent_id: int) -> tuple:
+    def step(self, action_ids: List[int], agent_id: int) -> Tuple(Tuple(np.ndarray, np.ndarray),
+                                                                  np.ndarray,
+                                                                  np.ndarray,
+                                                                  None):
         """Take one step in all game states.
         Args:
             action_ids -- list with ids of the actions for each state.
@@ -24,122 +26,108 @@ class HanabiParallelEnvironment:
             a tuple consisting of:
               - observation tuple (vectorized observation, legal moves)
               - reward array
-              - done (always False)
+              - done array
               - info (always None)
         """
 
-        last_score = self._parallel_env.last_observation.rewards.copy()
+        last_score = self._parallel_env.last_observation.scores.copy()
         # Apply the action to the state.
         self._parallel_env.apply_batch_move(action_ids, agent_id)
 
-        #  done = self.state.is_terminal()
+        # Observe next agent
+        self._parallel_env.observe_agent((agent_id + 1) % self.n_players)
+
         # Reward is score differential. May be large and negative at game end.
-        reward = self._parallel_env.last_observation.rewards - last_score
-        #  info = {}
+        reward = self._parallel_env.last_observation.scores - last_score
 
         return ((self._parallel_env.last_observation.batch_observation.copy(),
                  self._parallel_env.last_observation.legal_moves.copy()),
-                reward.copy(),
-                False, # the enviroment resets automatically.
+                reward,
+                self._parallel_env.last_observation.done.copy(),
                 None) # no additional info
 
-    def reset(self) -> tuple:
-        """Resets the environment for a new game.
+    @property
+    def game_config(self):
+        """Gather some game configuration details.
+        It includes
+          -- hand size,
+          -- number of cards,
+          -- number of colors,
+          -- number of ranks,
+          -- number of like tokens,
+          -- number of information token.
+        """
+        hand_size = self._parallel_env.parent_game.hand_size()
+        n_cards = self._parallel_env.parent_game.num_colors() \
+                * self._parallel_env.parent_game.cards_per_color()
+        n_colors = self._parallel_env.parent_game.num_colors()
+        n_ranks = self._parallel_env.parent_game.num_ranks()
+        n_life_tokens = self._parallel_env.parent_game.max_life_tokens()
+        n_info_tokens = self._parallel_env.parent_game.max_information_tokens()
+        return {"hand_size" : hand_size,
+                "n_cards" : n_cards,
+                "n_colors" : n_colors,
+                "n_ranks" : n_ranks,
+                "n_life" : n_life_tokens,
+                "n_info" : n_info_tokens,
+                }
+
+    def reset_terminal_states(self, current_agent_id: int) -> Tuple(np.ndarray, np.ndarray):
+        """Resets the terminal states and returns upated observations.
+
+        Args:
+            current_agent_id -- id of an agent to perform next move.
+
         Returns:
             observation: (vectorized observation, legal moves)
         """
-        self._parallel_env.new_initial_observation()
+        self._parallel_env.reset_terminal_states(current_agent_id)
+        self._parallel_env.observe_agent(current_agent_id)
         return (self._parallel_env.last_observation.batch_observation.copy(),
                 self._parallel_env.last_observation.legal_moves.copy())
 
-class HanabiDMEnv(dm_env.Environment):
-    """Hanabi environment wrapper for use with HanabiGameManager.
-    """
-    def __init__(self, config):
-        assert isinstance(config, dict), "Expected config to be of type dict."
-        self.game = pyhanabi.HanabiGame(config)
-        self.observation_encoder = pyhanabi.ObservationEncoder(
-            self.game, pyhanabi.ObservationEncoderType.CANONICAL)
-        self.n_players = self.game.num_players()
-        self.state = None
-        #  super(HanabiEnvironment, self).__init__(env_config)
+    def reset(self) -> Tuple(np.ndarray, np.ndarray):
+        """Resets the environment for a new game. Should be called once after
+        the instatiation of this env to retrieve initial observations.
 
-    def step(self, action_id: int) -> dm_env.TimeStep:
-        """Take one step in the game
-        Overrides the step() method from rl_env.HanabiEnv.
-        Breaking changes:
-          -- Observations contain only vectorized representations.
-          -- Action has to be an integer (i.e. action_id).
-        """
-
-        # fetch a move corresponding to action id.
-        action = self.game.get_move(action_id)
-        last_score = self.state.score()
-        # Apply the action to the state.
-        self.state.apply_move(action)
-
-        while self.state.cur_player() == pyhanabi.CHANCE_PLAYER_ID:
-            self.state.deal_random_card()
-
-        #  hanabi_observations = self._make_observation_all_players()
-        cur_hanabi_observation = self.state.observation(self.state.cur_player())
-        #  done = self.state.is_terminal()
-        # Reward is score differential. May be large and negative at game end.
-        reward = float(self.state.score() - last_score)
-        #  info = {}
-
-        if self.state.is_terminal():
-            return dm_env.termination(reward, self._observation(cur_hanabi_observation))
-        return dm_env.transition(reward, self._observation(cur_hanabi_observation))
-
-    def reset(self) -> dm_env.TimeStep:
-        """Resets the environment for a new game.
         Returns:
-            observation: vectorized observation
+            observation: (vectorized observation, legal moves)
         """
-        self.state = self.game.new_initial_state()
-        while self.state.cur_player() == pyhanabi.CHANCE_PLAYER_ID:
-            self.state.deal_random_card()
-        return dm_env.restart(self._observation(self.state.observation(self.state.cur_player())))
+        self._parallel_env.reset()
+        return (self._parallel_env.last_observation.batch_observation.copy(),
+                self._parallel_env.last_observation.legal_moves.copy())
 
-    def observation_spec(self) -> dict:
-        """Returns the observation spec."""
-        return {"observation" : specs.BoundedArray(shape=self.observation_encoder.shape(),
-                                                   dtype=np.float32,
-                                                   name="current_player_observation",
-                                                   minimum=0.0, maximum=1.0),
-                "legal_moves": specs.BoundedArray(shape=(self.game.max_moves(),), dtype=np.int,
-                                                  name="legal_moves", minimum=0,
-                                                  maximum=self.game.max_moves())}
+    @property
+    def max_moves(self):
+        """Total number of possible moves"""
+        return self._parallel_env.parent_game.max_moves()
 
-    def action_spec(self) -> specs.DiscreteArray:
+    @property
+    def observation_len(self):
+        """length of the vectorized observation of a single state"""
+        return self._parallel_env.observation_len()
+
+    @property
+    def num_states(self):
+        """Number of parallel states"""
+        return self._parallel_env.num_states()
+
+    def observation_spec(self) -> Tuple(dm_specs.BoundedArray, dm_specs.BoundedArray):
+        """Returns the observation spec.
+        Observation is a tuple containing observation and legal moves.
+        """
+        return (dm_specs.BoundedArray(shape=(self.num_states, self.observation_len),
+                                      dtype=np.int8,
+                                      name="agent_observation",
+                                      minimum=0, maximum=1),
+                dm_specs.BoundedArray(shape=(self.num_states, self.max_moves),
+                                      dtype=np.int8,
+                                      name="legal_moves",
+                                      minimum=0, maximum=1))
+
+    def action_spec(self) -> dm_specs.BoundedArray:
         """Returns the action spec."""
-        return specs.DiscreteArray(num_values=self.game.max_moves(), dtype=np.int, name="action")
-
-    def _make_observation_all_players(self):
-        """Make observation for all players.
-        Returns:
-        dict, containing observations for all players.
-        """
-        player_observations = [self.state.observation(pid) for pid in range(self.n_players)]
-        #  obs["player_observations"] = player_observations
-        #  obs["current_player"] = self.state.cur_player()
-        return player_observations
-
-    def _observation(self, hanabi_observation):
-        #  lmove_uids = [self.game.get_move_uid(move) for move in hanabi_observation.legal_moves()]
-        #  legal_moves = np.full((self.game.max_moves(),), -np.inf, dtype=np.float32)
-        #  legal_moves[lmove_uids] = 0.0
-        return {"observation": self._encode_observation(hanabi_observation),
-                "legal_moves": [self.game.get_move_uid(move)
-                                for move in hanabi_observation.legal_moves()]}
-
-    def _encode_observation(self, hanabi_observation) -> np.ndarray:
-        """Encode an observation.
-
-        Args:
-            observation (pyhanabi.Observation) -- an observation to encode.
-
-        Returns and encoded observation.
-        """
-        return np.array(self.observation_encoder.encode(hanabi_observation), dtype=np.float32)
+        return dm_specs.BoundedArray(shape=(self.num_states,),
+                                     dtype=np.int,
+                                     name="actions",
+                                     minimum=0, maximum=self.max_moves)
