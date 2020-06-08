d4be6a94208d9fa034208edb18e0ea131347d91f
Anton Komissarov <anton.v.komissarov@gmail.com>
2020-06-08 22:56:00 +0200

    update the examples according to the latest changes

--- examples/mock_agent_session.py
+++ examples/mock_agent_session.py
@@ -1,20 +1,54 @@
+"""
+An example with a mock agent on how to operate the framework.
+"""
 import hanabi_multiagent_framework as hmf
 from hanabi_multiagent_framework.utils import make_hanabi_env_config
 import numpy as np
+from numpy import ndarray
 from collections import namedtuple
 
 n_players = 2
-n_parallel = 1024
-env_conf, nb_actions, obs_shape = make_hanabi_env_config('Hanabi-Full', n_players)
+n_parallel = 10
+env_conf = make_hanabi_env_config('Hanabi-Full-Oracle', n_players)
 
-class MockAgent:
+env = hmf.HanabiParallelEnvironment(env_conf, n_parallel)
 
-    def act(self, observation, legal_moves):
+class MockAgent(hmf.agent.HanabiAgent):
+    """A mock agent which always selects the first legal move.
+    """
+
+    def explore(self, observations: ndarray, legal_moves: ndarray) -> ndarray:
         action = np.argmax(legal_moves, axis=1)
         return action
 
+    def exploit(self, observations: ndarray, legal_moves: ndarray) -> ndarray:
+        return self.explore(None, legal_moves)
+
+    def add_experience_first(self,
+                             observations: ndarray,
+                             legal_moves: ndarray,
+                             step_types: ndarray) -> None:
+        pass
+
+    def add_experience(self,
+                       observations: ndarray,
+                       legal_moves: ndarray,
+                       actions: ndarray,
+                       rewards: ndarray,
+                       step_types: ndarray) -> None:
+        pass
+
+    def update(self):
+        pass
+
+
 agents = [MockAgent() for _ in range(n_players)]
 
-parallel_session = hmf.HanabiParallelSession(agents, env_conf, n_parallel)
+parallel_session = hmf.HanabiParallelSession(env, agents)
+parallel_session.reset()
 
-parallel_session.run(1000)
+parallel_session.train(n_iter=30000,
+                       n_sim_steps=n_players,
+                       n_train_steps=1,
+                       n_warmup=0,
+                       train_batch_size=256)
--- examples/rlax_agent_session.py
+++ examples/rlax_agent_session.py
@@ -5,10 +5,10 @@ from collections import namedtuple
 from hanabi_agents.rlax_dqn import DQNAgent
 
 n_players = 2
-n_parallel = 1_000
+n_parallel = 32
 epochs = 1_000_000
-eval_n_iter = 1_000
-eval_freq = 3_000
+eval_n_parallel = 1_000
+eval_freq = 300
 
 def create_scheduler(eps_start, eps_min, steps1, steps2):
     def scheduler(x):
@@ -22,24 +22,25 @@ def create_scheduler(eps_start, eps_min, steps1, steps2):
 
 schedule = create_scheduler(0.9, 0.05, 200_000, 2_000_000)
 
-env_conf, nb_actions, obs_shape = make_hanabi_env_config('Hanabi-Small-Oracle', n_players)
-#  env_conf, nb_actions, obs_shape = make_hanabi_env_config('Hanabi-Full-Oracle', n_players)
-env_conf["max_life_tokens"] = 3
-obs_shape[0] += env_conf["max_life_tokens"] - 1
+env_conf = make_hanabi_env_config('Hanabi-Full-Oracle', n_players)
 
-self_play_agent = DQNAgent(obs_shape, nb_actions,
+env = hmf.HanabiParallelEnvironment(env_conf, n_parallel)
+eval_env = hmf.HanabiParallelEnvironment(env_conf, eval_n_parallel)
+#  env_conf["max_life_tokens"] = 3
+#  obs_shape[0] += env_conf["max_life_tokens"] - 1
+
+self_play_agent = DQNAgent(env.observation_spec_vec()[0], env.action_spec(),
                            target_update_period=500,
-                           learning_rate=1e-5,
+                           learning_rate=5e-6,
                            epsilon=schedule,
                            layers=[512, 512])
+
 #  agents = [DQNAgent(obs_shape, nb_actions) for _ in range(n_players)]
 agents = [self_play_agent for _ in range(n_players)]
 
-parallel_session = hmf.HanabiParallelSession(agents, env_conf, n_parallel,
-                                             exp_buffer_size=5_000 * n_parallel)
+parallel_session = hmf.HanabiParallelSession(env, agents)
 
-parallel_eval_session = hmf.HanabiParallelSession(agents, env_conf, n_states=eval_n_iter,
-                                                  exp_buffer_size=0)
+parallel_eval_session = hmf.HanabiParallelSession(eval_env, agents)
 
 print("Game config", parallel_session.parallel_env.game_config)
 
@@ -49,9 +50,9 @@ parallel_eval_session.run_eval()
 # train
 parallel_session.train(n_iter=eval_freq,
                        n_sim_steps=n_players,
-                       n_train_steps=10,
+                       n_train_steps=2,
                        n_warmup=10,
-                       train_batch_size=32)
+                       train_batch_size=256)
 
 print("step", 1 * eval_freq)
 # eval
@@ -60,9 +61,9 @@ parallel_eval_session.run_eval()
 for i in range(epochs // eval_freq):
     parallel_session.train(n_iter=eval_freq,
                            n_sim_steps=n_players,
-                           n_train_steps=4,
+                           n_train_steps=2,
                            n_warmup=0,
-                           train_batch_size=32)
+                           train_batch_size=256)
     print("step", (i + 2) * eval_freq)
     # eval after
     parallel_eval_session.run_eval()
