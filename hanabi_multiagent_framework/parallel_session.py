a798267f8adaab1fdd763b08cc373fe732553866
Anton Komissarov <anton.v.komissarov@gmail.com>
2020-06-08 22:42:08 +0200

    further simplify the parallel session: let the agents handle the data consolidation

--- hanabi_multiagent_framework/parallel_session.py
+++ hanabi_multiagent_framework/parallel_session.py
@@ -103,90 +103,56 @@ class HanabiParallelSession:
 
     def run(self, n_steps: int):
         """Make <n_steps> in each of the parallel game states.
+        States, rewards, etc. are preserved between runs.
         """
-        dummy_obs = np.zeros((self.n_states, self.obs_len))
-        dummy_lms = np.ones((self.n_states, self.max_moves))
         total_reward = np.zeros(self.n_states)
-        cur_step = 1
-        agent_obs_tm1 = np.full(((len(self.agents)), self.n_states, self.obs_len), np.nan)
-        agent_act_tm1 = np.empty(((len(self.agents)), self.n_states, 1))
-        agent_obs_t = np.full(((len(self.agents)), self.n_states, self.obs_len), np.nan)
-        agent_act_t = np.empty(((len(self.agents)), self.n_states, 1))
-        agent_lms_t = np.empty(((len(self.agents)), self.n_states, self.max_moves))
-        agent_reward_tm1 = np.zeros((len(self.agents), self.n_states, 1))
-        agent_reward_t = np.zeros((len(self.agents), self.n_states, 1))
-        def handle_terminal_states(terminal, reward, agent_id):
-            terminal = terminal == 1
-            agent_reward_t[:, terminal] = 0
-            self.experience[agent_id].add_transition(
-                agent_obs_t[agent_id, terminal, :],
-                agent_act_t[agent_id, terminal, :],
-                dummy_obs[terminal, :],
-                dummy_lms[terminal, :],
-                reward[terminal].reshape((-1, 1)),
-                np.full((np.sum(terminal),), True))
-            agent_obs_tm1[:, terminal, :] = np.nan
-            self._cur_obs, self._cur_lm = self.parallel_env.reset_terminal_states(
-                (agent_id + 1) % len(self.agents))
+        cur_step = 0
+        #  step_types = self.parallel_env.step_types
 
+        def handle_terminal_states(step_types, agent_id):
+            terminal = step_types == StepType.LAST
+            (self._cur_obs, self._cur_lm), step_types = self.parallel_env.reset_states(
+                np.nonzero(terminal)[0],
+                agent_id)
+            agent.add_experience_first(self._cur_obs, self._cur_lm, step_types)
 
         while cur_step < n_steps:
             # beginning of the agent's turn.
             agent_id, agent = self.agents.next()
 
-
-            # collect the outcome of the last round.
-            # it includes the agents last observation_t (not current!),
-            # corresponding action_t, and reward_t.
-            # agent also has to know the observation_tm1 and action_tm1
-
-            valid_transitions = np.logical_not(np.any(np.isnan(agent_obs_tm1[agent_id]), axis=1))
-            self.experience[agent_id].add_transition(
-                agent_obs_tm1[agent_id, valid_transitions, :],
-                agent_act_tm1[agent_id, valid_transitions, :],
-                agent_obs_t[agent_id, valid_transitions, :],
-                agent_lms_t[agent_id, valid_transitions, :],
-                agent_reward_tm1[agent_id, valid_transitions, :],
-                np.full((np.sum(valid_transitions),), False))
-                #  agent_terminal_t[agent_id, valid_transitions])
-
-            # record new observation and legal actions
-            agent_obs_tm1[agent_id] = agent_obs_t[agent_id]
-            agent_obs_t[agent_id] = self._cur_obs
-            agent_lms_t[agent_id] = self._cur_lm
+            handle_terminal_states(self.parallel_env.step_types, agent_id)
 
             # agent acts
-            _, actions = agent.explore(self._cur_obs, self._cur_lm)
-            # generate actions again if the agent produced illegal actions.
-            while not np.all(self._cur_lm[range(len(actions)), actions]):
-                print("Illegal action detected. Re-generating actions.")
-                _, actions = agent.explore(self._cur_obs, self._cur_lm)
-
-            # record agent's actions.
-            agent_act_tm1[agent_id] = agent_act_t[agent_id]
-            agent_act_t[agent_id, :, 0] = actions
+            actions = agent.explore(self._cur_obs, self._cur_lm)
 
             # apply actions to the states and get new observations, rewards, statuses.
-            (self._cur_obs, self._cur_lm), rewards, done, _, = self.parallel_env.step(
-                list(actions), agent_id)
+            (self._cur_obs, self._cur_lm), rewards, step_types = self.parallel_env.step(
+                actions, agent_id)
 
-            #  rewards = np.clip(rewards, 0, None)
-            #  rewards = np.abs(rewards)
-
-            handle_terminal_states(done, rewards, agent_id)
-            contiguous_states = done == 0
-
-            # reward is cumulative over the curse of the whole round.
+            # reward is cumulative over the course of the whole round.
             # i.e. the agents gets a reward for his action as well as for the
             # actions of its co-players.
-
-            # agent's reward should not be updated if the state has reached its terminus
-            # during the last round
-            agent_reward_tm1[agent_id] = agent_reward_t[agent_id]
-            agent_reward_t[agent_id, :] = 0
-            agent_reward_t[:, contiguous_states] += np.broadcast_to(
+            self.agent_cum_rewards[:, self.agent_contiguous_states] += np.broadcast_to(
                 rewards.reshape((-1, 1)),
-                agent_reward_t.shape)[:, contiguous_states]
+                self.agent_cum_rewards.shape)[self.agent_contiguous_states]
+
+            # set the terminal states as not contiguous for all agents
+            self.agent_contiguous_states[:, step_types == 2] = False
+            # reset contigency of all states for current agent
+            self.agent_contiguous_states[agent_id, :] = True
+
+            # add new experiences to the agent
+            agent.add_experience(
+                self._cur_obs,
+                self._cur_lm,
+                actions,
+                self.agent_cum_rewards[agent_id],
+                step_types)
+
+            # reset the cumulative reward for the current agent
+            self.agent_cum_rewards[agent_id, :] = 0
+
+            total_reward += rewards
 
             cur_step += 1
         return cur_step, total_reward
@@ -196,21 +162,19 @@ class HanabiParallelSession:
               n_iter: int,
               n_sim_steps: int,
               n_train_steps: int,
-              n_warmup: int,
-              train_batch_size: int):
+              n_warmup: int):
         """Train agents.
 
         Args:
-            n_epochs -- number of training iteration.
-            n_steps -- number of steps to run in each iteration.
+            n_iter -- number of training iteration.
+            n_sim_steps -- number of game steps to run in each training iteration.
+            n_train_steps -- number of agents' training updates per training iteration.
             n_warmup -- number of steps to run before the training starts
-                        (to fill the experience buffer)
-            train_batch_size -- training batch size.
+                        (e.g. to fill the experience buffer)
         """
         self.run(n_warmup)
         for _ in range(n_iter):
             self.run(n_sim_steps)
             for _ in range(n_train_steps):
-                for agent_id, agent in enumerate(self.agents.agents):
-                    exp = self.experience[agent_id].sample(train_batch_size)
-                    agent.train(*exp)
+                for agent in self.agents.agents:
+                    agent.update()
